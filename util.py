b3072ee862d5f4de3fdea19538d4b963a5d83a82
Ben Kraft <benkraft@khanacademy.org>
2017-04-17 10:12:57 -0700

    Add util to get channel ID

--- util.py
+++ util.py
@@ -17,9 +17,16 @@ def call_api(call, data=None):
         raise SlackNotOkayException(res.get('error', str(res)))
 
 
-def send_as_user(channel, text):
+def send_as_user(channel_name, text):
     return call_api('chat.postMessage', {
-        'channel': channel,
+        'channel': channel_name,
         'text': text,
         'as_user': 'true',
     })
+
+
+def channel_id(channel_name):
+    channels = call_api('channels.list')
+    for channel in channels['channels']:
+        if '#' + channel['name'] == channel_name:
+            return channel['id']
